bded7cb0a633bf8a2e3ec7f850a2d070734c9eda
rafiihyas <158743406+rafiihyas@users.noreply.github.com>
2026-04-19 03:52:07 +0800

    style: improve hero section ui

--- src/app/(main)/components/sections/Hero.tsx
+++ src/app/(main)/components/sections/Hero.tsx
@@ -5,9 +5,9 @@ export default function Hero() {
   return (
     <section
       id="hero"
-      className="relative flex flex-col lg:flex-row justify-center lg:justify-start items-center lg:items-stretch min-h-screen px-6 md:px-12 lg:px-24 xl:px-52 pt-28 lg:pt-0 overflow-hidden"
+      className="relative flex flex-col lg:flex-row justify-center lg:justify-start items-center lg:items-stretch min-h-screen px-6 md:px-12 lg:px-24 xl:px-52 pt-10 lg:pt-0 overflow-hidden"
     >
-      <div className="flex flex-col space-y-6 h-full items-start justify-center w-full lg:max-w-[750px] z-10 mt-10 lg:mt-52">
+      <div className="flex flex-col space-y-6 h-full items-start justify-center w-full lg:max-w-[750px] z-10 lg:mt-72">
         <h1 className="text-4xl md:text-[50px] text-kreafi-2 font-semibold leading-tight">
           Turning Complex Logic into
           <br className="hidden md:block" />
@@ -21,7 +21,7 @@ export default function Hero() {
           dengan sempurna di balik layar, tapi juga memanjakan mata pengguna.
           Frontend Developer & Graphic Designer berbasis di Makassar.
         </p>
-        <div className="flex space-x-4 pt-2">
+        <div className="flex space-x-4 pt-2 items-start w-full">
           <Button className="bg-kreafi-1 hover:bg-kreafi-1/90 text-kreafi-2 text-sm font-semibold shadow px-6">
             Download CV
           </Button>
@@ -34,7 +34,7 @@ export default function Hero() {
         </div>
       </div>
 
-      <div className="relative lg:absolute z-0 right-0 bottom-0 mt-12 lg:mt-0 flex justify-end w-full max-w-[300px] md:max-w-[450px] lg:max-w-[618px]">
+      <div className="relative lg:absolute z-0 right-0 bottom-0 mt-12 lg:mt-0 md:flex justify-end w-full hidden max-w-[300px] md:max-w-[450px] lg:max-w-[618px]">
         <Image
           src="/assets/foto-rafi-1.svg"
           alt="foto rafi 1"
